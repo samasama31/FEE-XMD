57d0f497f9ccabb03db28ee301b7a99aebc685b1
Fredi AI <frediezra60@gmail.com>
2026-01-22 23:06:02 +0300

    Update settings.js

--- Env/settings.js
+++ Env/settings.js
@@ -1,6 +1,6 @@
  const session = process.env.SESSION || '';
 const mycode = process.env.CODE || "255";
-const botname = process.env.BOTNAME || '🄵🄴🄴-🅇🄼🄳';
+const botname = process.env.BOTNAME || '𝙁𝙀𝙀-𝙓𝙈𝘿';
 const herokuAppName = process.env.HEROKU_APP_NAME || '';
 const herokuApiKey = process.env.HEROKU_API_KEY || '';
 const database = process.env.DATABASE_URL || '';
